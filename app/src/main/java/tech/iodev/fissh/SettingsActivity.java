efc0d21dd89280ea106fedbd375847c6e5873bdb
Suvansh Bansal <suvansh@uginfosystems.com>
2018-02-27 18:01:07 +0530

    Added spaces and Removing the error for non-empty ip

--- app/src/main/java/tech/iodev/fissh/SettingsActivity.java
+++ app/src/main/java/tech/iodev/fissh/SettingsActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -31,14 +34,62 @@ public class SettingsActivity extends AppCompatActivity {
         nickname.setText(getIntent().getStringExtra("nickname"));
 
         ID = getIntent().getIntExtra("id", -1);
+
+        computerIP.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+
+                if(computerIP.getText().toString().equals(""))
+                {
+                    computerIP.setError("Please input the ip of your computer");
+                }
+
+            }
+        });
+
+        computerIP.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if(computerIP.getText().toString().equals(""))
+                {
+                    computerIP.setError("Please input the ip of your computer");
+                }
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if(computerIP.getText().toString().equals(""))
+                {
+                    computerIP.setError("Please input the ip of your computer");
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                if(computerIP.getText().toString().equals(""))
+                {
+                    computerIP.setError("Please input the ip of your computer");
+                }
+
+            }
+        });
     }
 
     public void save_settings(View sender)
     {
         if(computerIP.getText().toString().equals(""))
+        {
+
             computerIP.setError("Please input the ip of your computer");
 
-        else {
+        }
+        else
+        {
             Intent result = new Intent();
             result.putExtra("computer_ip", computerIP.getText().toString());
             result.putExtra("password", password.getText().toString());
